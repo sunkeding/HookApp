ad81f1cb1ce12650a5a7982a9782236d11beeb7b
skd <970335012@qq.com>
2019-06-30 21:42:22 +0800

    Hook startActivity方法

---
+++ app/src/main/java/com/keding/hookapplication/SecondActivity.java
@@ -0,0 +1,20 @@
+package com.keding.hookapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * @author: skd
+ * @date 2019-06-30
+ * @Desc SecondActivity
+ */
+public class SecondActivity extends Activity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+    }
+}
